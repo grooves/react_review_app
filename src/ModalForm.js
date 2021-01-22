3a54e428e60580e40e81e1fe61b6cebb580e4b0d
Masamichi Kusaba <m.kusaba6@gmail.com>
2021-01-22 16:31:56 +0900

    Done

--- src/ModalForm.js
+++ src/ModalForm.js
@@ -1,24 +1,45 @@
+import { Box, Button, Form, FormField, RangeInput, TextArea, TextInput } from "grommet";
+
 export function ModalForm(props) {
   return(
-    <div>
-      <div>
-        <label htmlFor='title'>タイトル</label>
-        <input type='text' id='title' maxLength='255' value={props.formValues.title} onChange={props.setFormValue}/>
-      </div>
-      <div>
-        <label htmlFor='score'>評価</label>
-        <input type='number' id='score' min='1' max='5' value={props.formValues.score} onChange={props.setFormValue}/>
-      </div>
-      <div>
-        <label htmlFor='body'>本文</label>
-        <input type='text' id='body' value={props.formValues.body} onChange={props.setFormValue}/>
-      </div>
-      <div>
-        <label htmlFor='reviewer'>レビュワー</label>
-        <input type='text' id='reviewer' maxLength='255' value={props.formValues.reviewer} onChange={props.setFormValue}/>
-      </div>
-      <button onClick={props.closeModal}>キャンセル</button>
-      <input type='button' value='送信' onClick={props.submitForm} />
-    </div>
+    <Form>
+      <FormField htmlFor='title' label='タイトル'>
+        <TextInput
+          id='title'
+          maxLength='255'
+          value={props.formValues.title}
+          onChange={props.setFormValue}
+        />
+      </FormField>
+      <FormField htmlFor='body' label='本文'>
+        <TextArea
+          id='body'
+          size='xlarge'
+          value={props.formValues.body}
+          onChange={props.setFormValue}
+        />
+      </FormField>
+      <FormField htmlFor='score' label='評価'>
+        <RangeInput
+          id='score'
+          min='1'
+          max='5'
+          value={props.formValues.score}
+          onChange={props.setFormValue}
+        />
+      </FormField>
+      <FormField htmlFor='reviewer' label='レビュワー'>
+        <TextInput
+          id='reviewer'
+          maxLength='255'
+          value={props.formValues.reviewer}
+          onChange={props.setFormValue}
+        />
+      </FormField>
+      <Box direction='row' gap='medium'>
+        <Button label='キャンセル' onClick={props.closeModal}/>
+        <Button primary label='送信' onClick={props.submitForm}/>
+      </Box>
+    </Form>
   )
 }
--- src/Review.js
+++ src/Review.js
@@ -1,24 +1,38 @@
+import { Box, Button, Card, CardBody, CardFooter, CardHeader, Text } from "grommet";
+import { Edit, Favorite, Trash } from 'grommet-icons';
+
 export default function Review(props) {
   return (
-    <>
-      <table>
-        <thead>
-          <th>タイトル</th>
-          <th>本文</th>
-          <th>スコア</th>
-          <th>レビュワー</th>
-          <th></th>
-        </thead>
-        <tbody>
-          <td>{props.review.title}</td>
-          <td>{props.review.body}</td>
-          <td>{props.review.score}</td>
-          <td>{props.review.reviewer}</td>
-          <td>
-            <button onClick={props.openModal}>編集</button>
-          </td>
-        </tbody>
-      </table>
-    </>
+    <Card height="small" width="medium" background="light-1" margin='small'>
+      <CardHeader pad="medium">
+        <Text weight='bold' size='large'>
+          {props.review.title}
+        </Text>
+      </CardHeader>
+      <CardBody pad="medium">
+        {props.review.body}
+      </CardBody>
+      <CardFooter pad={{horizontal: "small"}} background="light-2">
+        <Box direction='row'>
+          {
+            [...Array(5)].map((_, i) => (
+              <Favorite
+                key={i}
+                color={
+                  i < props.review.score ? 'accent-2' : ''
+                }
+              />
+            ))
+          }
+        </Box>
+        <Text truncate='10'>
+          {props.review.reviewer}
+        </Text>
+        <Box direction='row'>
+          <Button icon={<Edit color='accent-1'/>} onClick={() => { props.openAsEdit(props.review) }}/>
+          <Button icon={<Trash color='accent-2'/>} onClick={ () => {props.deleteReview(props.review.id)} }/>
+        </Box>
+      </CardFooter>
+    </Card>
   )
 }
